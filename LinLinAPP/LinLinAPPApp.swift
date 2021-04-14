00f87c8fe678ce4e74c79e1b6f0a7aa802b2da42
Linlin <chenglin353163@gmail.com>
2021-04-14 12:00:05 +0800

    Initial Commit

---
+++ LinLinAPP/LinLinAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinLinAPPApp.swift
+//  LinLinAPP
+//
+//  Created by Linlin on 2021/4/14.
+//
+
+import SwiftUI
+
+@main
+struct LinLinAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
